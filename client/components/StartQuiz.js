30060972671c24fc2a28815144639044575c5849
Ryan Cokely <ryan.cokely@gmail.com>
2023-08-18 14:43:50 -0400

    creating quiz sessions

--- client/components/StartQuiz.js
+++ client/components/StartQuiz.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
 const StartQuiz = () => {
-  const [socket] = useState(io.connect('http://localhost:3000')); // Replace with your server URL
-  const [sessionId] = useState('some-session-id'); // Replace with your session management logic
+  const [socket, setSocket] = useState(null);
+  const [sessionId, setSessionId] = useState('');
+
+  useEffect(() => {
+    const newSocket = io.connect('http://localhost:3000');
+    setSocket(newSocket);
+    return () => newSocket.disconnect();
+  }, [setSocket]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('sessionCreated', (newSessionId) => {
+        setSessionId(newSessionId);
+      });
+    }
+  }, [socket]);
+
+  const createSession = () => {
+    const newSessionId = uuidv4();
+    socket.emit('createSession', newSessionId);
+  };
 
   const startQuiz = () => {
     socket.emit('startQuiz', sessionId);
@@ -11,7 +31,13 @@ const StartQuiz = () => {
 
   return (
     <div className="start-quiz">
-      <button onClick={startQuiz}>Start Quiz</button>
+      <button onClick={createSession}>Create Quiz Session</button>
+      {sessionId && (
+        <>
+          <p>Session ID: {sessionId}</p>
+          <button onClick={startQuiz}>Start Quiz</button>
+        </>
+      )}
     </div>
   );
 };
--- server/app.js
+++ server/app.js
@@ -1,46 +1,3 @@
-// const path = require('path')
-// const express = require('express')
-// const morgan = require('morgan')
-// const app = express()
-// module.exports = app
-
-// // logging middleware
-// app.use(morgan('dev'))
-
-// // body parsing middleware
-// app.use(express.json())
-
-// // auth and api routes
-// app.use('/auth', require('./auth'))
-// app.use('/api', require('./api'))
-
-// app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
-
-// // static file-serving middleware
-// app.use(express.static(path.join(__dirname, '..', 'public')))
-
-// // any remaining requests with an extension (.js, .css, etc.) send 404
-// app.use((req, res, next) => {
-//   if (path.extname(req.path).length) {
-//     const err = new Error('Not found')
-//     err.status = 404
-//     next(err)
-//   } else {
-//     next()
-//   }
-// })
-
-// // sends index.html
-// app.use('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-// })
-
-// // error handling endware
-// app.use((err, req, res, next) => {
-//   console.error(err)
-//   console.error(err.stack)
-//   res.status(err.status || 500).send(err.message || 'Internal server error.')
-// })
 
 const path = require('path');
 const express = require('express');
@@ -108,3 +65,4 @@ app.use((err, req, res, next) => {
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
